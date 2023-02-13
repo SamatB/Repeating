bef5ce01d316c50655990395e19406cd666527c5
samat <samhardy1927@gmail.com>
2023-02-13 16:32:01 +0300

    Queue

--- src/main/java/org/example/collection/queue/PriorityQ.java
+++ src/main/java/org/example/collection/queue/PriorityQ.java
@@ -4,6 +4,10 @@ import java.util.PriorityQueue;
 
 public class PriorityQ {
     public static void main(String[] args) {
+        ints();
+    }
+
+    public static void op() {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         priorityQueue.add(1);
         priorityQueue.add(2);
@@ -28,4 +32,18 @@ public class PriorityQ {
         System.out.println("Peeking : " + priorityQueue.peek());
         System.out.println("Elementing : " + priorityQueue.element());
     }
+
+    public static void ints() {
+        PriorityQueue<Integer> integers = new PriorityQueue<>();
+        integers.add(10);
+        integers.add(1);
+        integers.add(15);
+        integers.add(5);
+        integers.add(8);
+        System.out.println(integers);
+        System.out.println(integers.peek());
+        System.out.println(integers.contains(3));
+    }
+
+
 }
